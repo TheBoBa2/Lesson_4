9a1523f3133678430c543d454fece61fa0a5620f
TheBoBa2 <glooplootloo@gmail.com>
2026-06-04 14:49:10 +0300

    updated 02 task

--- 02_findSum.py
+++ 02_findSum.py
@@ -1,3 +1,6 @@
 ﻿input_lst = list(map(int, input("Введіть значення: ").split()))
 
-print(sum(input_lst[::2] * input_lst[-1]))
+if len(input_lst) == 0:
+    print(0)
+else:
+    print(sum(input_lst[::2] * input_lst[-1]))
