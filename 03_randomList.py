19b3a559b6a36222d09bf64159b21e0bc2257529
TheBoBa2 <glooplootloo@gmail.com>
2026-06-05 22:24:54 +0300

    updated 03 task

---
+++ 03_randomList.py
@@ -0,0 +1,12 @@
+﻿import random
+
+random_lst = []
+amount = int(random.randint(3, 10))
+
+while amount > 0:
+    random_lst.append(random.randint(1, 10))
+    amount -= 1
+
+result = [random_lst[0], random_lst[2], random_lst[-2]]
+
+print(random_lst, " == ", result)
